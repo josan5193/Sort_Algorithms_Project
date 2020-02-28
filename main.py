d9ab3d6b2c3a401224e9befb28b28291b369ff0d
shines76 <shines4@cub.uca.edu>
2020-02-27 23:36:53 -0600

    commit 2
    sorry had some other hw, I put merge and bubble in but I don't know if it is exactly what we want yet

--- main.py
+++ main.py
@@ -71,6 +71,111 @@ def insertion_sort(arr):
     write2file(INSE, str(insertion_comparison))
     return arr
 
+#From geeks for geeks
+# Python program for implementation of Bubble Sort
+def bubbleSort(arr):
+	n = len(arr)
+
+	# Traverse through all array elements
+	for i in range(n):
+
+		# Last i elements are already in place
+		for j in range(0, n-i-1):
+
+			# traverse the array from 0 to n-i-1
+			# Swap if the element found is greater
+			# than the next element
+			if arr[j] > arr[j+1] :
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+
+# Driver code to test above
+arr = [64, 34, 25, 12, 22, 11, 90]
+
+bubbleSort(arr)
+
+print ("Sorted array is:")
+for i in range(len(arr)):
+	print ("%d" %arr[i]), 
+
+
+# Python program for implementation of MergeSort(geeks for geeks)
+
+# Merges two subarrays of arr[]. 
+# First subarray is arr[l..m] 
+# Second subarray is arr[m+1..r] 
+def merge(arr, l, m, r): 
+	n1 = m - l + 1
+	n2 = r- m 
+
+	# create temp arrays 
+	L = [0] * (n1) 
+	R = [0] * (n2) 
+
+	# Copy data to temp arrays L[] and R[] 
+	for i in range(0 , n1): 
+		L[i] = arr[l + i] 
+
+	for j in range(0 , n2): 
+		R[j] = arr[m + 1 + j] 
+
+	# Merge the temp arrays back into arr[l..r] 
+	i = 0	 # Initial index of first subarray 
+	j = 0	 # Initial index of second subarray 
+	k = l	 # Initial index of merged subarray 
+
+	while i < n1 and j < n2 : 
+		if L[i] <= R[j]: 
+			arr[k] = L[i] 
+			i += 1
+		else: 
+			arr[k] = R[j] 
+			j += 1
+		k += 1
+
+	# Copy the remaining elements of L[], if there 
+	# are any 
+	while i < n1: 
+		arr[k] = L[i] 
+		i += 1
+		k += 1
+
+	# Copy the remaining elements of R[], if there 
+	# are any 
+	while j < n2: 
+		arr[k] = R[j] 
+		j += 1
+		k += 1
+
+# l is for left index and r is right index of the 
+# sub-array of arr to be sorted 
+def mergeSort(arr,l,r): 
+	if l < r: 
+
+		# Same as (l+r)/2, but avoids overflow for 
+		# large l and h 
+		m = (l+(r-1))/2
+
+		# Sort first and second halves 
+		mergeSort(arr, l, m) 
+		mergeSort(arr, m+1, r) 
+		merge(arr, l, m, r) 
+
+
+# Driver code to test above 
+arr = [12, 11, 13, 5, 6, 7] 
+n = len(arr) 
+print ("Given array is") 
+for i in range(n): 
+	print ("%d" %arr[i]), 
+
+mergeSort(arr,0,n-1) 
+print ("\n\nSorted array is") 
+for i in range(n): 
+	print ("%d" %arr[i]), 
+
+# This code is contributed by Mohit Kumra 
+
+
 #This Algorithm Was Obtained From GeeksforGeeks.org 
 def heapify(arr, n, i): 
     largest = i  # Initialize largest as root 
