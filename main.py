488f3af6ce9706627eb062a8d2007a9feec32abb
shines76 <shines4@cub.uca.edu>
2020-02-28 15:29:58 -0600

    commit 3
    bubble and merge are up and running good.  im going to my parents house for tonight and tomorrow and ill work on getting that spreadsheet around midday.

--- main.py
+++ main.py
@@ -35,10 +35,11 @@ def sorted_array_WORST(size):
         arr.append(size - (i-1))
     return arr
 
-test1 = 1234
-test2 = 5678
-write2file(MERG, str(test1)) 
-write2file(MERG, str(test2))
+
+#write2file(MERG, str(test1)) 
+#write2file(MERG, str(test2))
+
+
 
 test3 = rand_array(10)
 print(test3)
@@ -52,6 +53,7 @@ print(test5)
 test6 = sorted_array_WORST(100)
 test7 = sorted_array_BEST(100)
 
+#python implementation for insertion sort
 def insertion_sort(arr):
         
     for i in range(len(arr)):
@@ -71,109 +73,60 @@ def insertion_sort(arr):
     write2file(INSE, str(insertion_comparison))
     return arr
 
-#From geeks for geeks
-# Python program for implementation of Bubble Sort
-def bubbleSort(arr):
-	n = len(arr)
+#python implemenation for bubble_sort
+def bubble_sort(arr):
+    def swap(i, j):
+        arr[i], arr[j] = arr[j], arr[i]
 
-	# Traverse through all array elements
-	for i in range(n):
-
-		# Last i elements are already in place
-		for j in range(0, n-i-1):
-
-			# traverse the array from 0 to n-i-1
-			# Swap if the element found is greater
-			# than the next element
-			if arr[j] > arr[j+1] :
-				arr[j], arr[j+1] = arr[j+1], arr[j]
-
-# Driver code to test above
-arr = [64, 34, 25, 12, 22, 11, 90]
-
-bubbleSort(arr)
-
-print ("Sorted array is:")
-for i in range(len(arr)):
-	print ("%d" %arr[i]), 
+    n = len(arr)
+    swapped = True
+    
+    x = -1
+    while swapped:
+        swapped = False
+        x = x + 1
+        for i in range(1, n-x):
+            if arr[i - 1] > arr[i]:
+                swap(i - 1, i)
+                swapped = True
+                    
+    return arr
 
 
-# Python program for implementation of MergeSort(geeks for geeks)
+# Python implementation of MergeSort
 
-# Merges two subarrays of arr[]. 
-# First subarray is arr[l..m] 
-# Second subarray is arr[m+1..r] 
-def merge(arr, l, m, r): 
-	n1 = m - l + 1
-	n2 = r- m 
+def merge_sort(arr):
+    # The last array split
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    # Perform merge_sort recursively on both halves
+    left, right = merge_sort(arr[:mid]), merge_sort(arr[mid:])
 
-	# create temp arrays 
-	L = [0] * (n1) 
-	R = [0] * (n2) 
-
-	# Copy data to temp arrays L[] and R[] 
-	for i in range(0 , n1): 
-		L[i] = arr[l + i] 
-
-	for j in range(0 , n2): 
-		R[j] = arr[m + 1 + j] 
-
-	# Merge the temp arrays back into arr[l..r] 
-	i = 0	 # Initial index of first subarray 
-	j = 0	 # Initial index of second subarray 
-	k = l	 # Initial index of merged subarray 
-
-	while i < n1 and j < n2 : 
-		if L[i] <= R[j]: 
-			arr[k] = L[i] 
-			i += 1
-		else: 
-			arr[k] = R[j] 
-			j += 1
-		k += 1
-
-	# Copy the remaining elements of L[], if there 
-	# are any 
-	while i < n1: 
-		arr[k] = L[i] 
-		i += 1
-		k += 1
-
-	# Copy the remaining elements of R[], if there 
-	# are any 
-	while j < n2: 
-		arr[k] = R[j] 
-		j += 1
-		k += 1
-
-# l is for left index and r is right index of the 
-# sub-array of arr to be sorted 
-def mergeSort(arr,l,r): 
-	if l < r: 
-
-		# Same as (l+r)/2, but avoids overflow for 
-		# large l and h 
-		m = (l+(r-1))/2
-
-		# Sort first and second halves 
-		mergeSort(arr, l, m) 
-		mergeSort(arr, m+1, r) 
-		merge(arr, l, m, r) 
+    # Merge each side together
+    return merge(left, right, arr.copy())
 
 
-# Driver code to test above 
-arr = [12, 11, 13, 5, 6, 7] 
-n = len(arr) 
-print ("Given array is") 
-for i in range(n): 
-	print ("%d" %arr[i]), 
+def merge(left, right, merged):
 
-mergeSort(arr,0,n-1) 
-print ("\n\nSorted array is") 
-for i in range(n): 
-	print ("%d" %arr[i]), 
+    left_cursor, right_cursor = 0, 0
+    while left_cursor < len(left) and right_cursor < len(right):
+      
+        # Sort each one and place into the result
+        if left[left_cursor] <= right[right_cursor]:
+            merged[left_cursor+right_cursor]=left[left_cursor]
+            left_cursor += 1
+        else:
+            merged[left_cursor + right_cursor] = right[right_cursor]
+            right_cursor += 1
+            
+    for left_cursor in range(left_cursor, len(left)):
+        merged[left_cursor + right_cursor] = left[left_cursor]
+        
+    for right_cursor in range(right_cursor, len(right)):
+        merged[left_cursor + right_cursor] = right[right_cursor]
 
-# This code is contributed by Mohit Kumra 
+    return merged
 
 
 #This Algorithm Was Obtained From GeeksforGeeks.org 
